1b934f706d3a52a91f455fd3297c9d80ff5e3021
Yao Zi <ziyao@disroot.org>
2025-01-25 20:23:41 +0000

    Fix off-by-one bug in extract-fw.lua
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- extract-fw.lua
+++ extract-fw.lua
@@ -31,9 +31,9 @@ assert(ilmLen and dlmLen and buildVer and fwVer and buildTime);
 pf("ilmlen: 0x%x (%d), dlmLen: 0x%x (%d)\n", ilmLen, ilmLen, dlmLen, dlmLen);
 pf("build version %d (0x%x), firmware version %d\n", buildVer, buildVer, fwVer);
 pf("build time string: %s\n", buildTime:gsub('_', ''));
-file = file:sub(32, -1);
+file = file:sub(33, -1);
 
-local ivb = file:sub(0, 0x40);
-local ilm = file:sub(0x40, 0x40 + ilmLen);
+local ivb = file:sub(1, 0x40);
+local ilm = file:sub(0x41, 0x40 + ilmLen);
 assert(io.open("ivb.bin", 'w')):write(ivb);
 assert(io.open("ilm.bin", 'w')):write(ilm);
